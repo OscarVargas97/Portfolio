5005b92ee179d5f7ca075b4e25c24d108f130b82
OscarVargas97 <oscarvargas97@gmail.com>
2022-08-23 13:39:29 -0400

    Modify base errors

--- stop-todo.sh
+++ stop-todo.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 cd docker
 if [[ $1 == "dev" ]]; then  
-  docker-compose --project-name "template"  --project-name --env-file ./docker/environments/mysql.env -f docker-compose.yml -f docker-compose.dev.yml stop --no-deps  
+  docker-compose --project-name "template"  --project-name --env-file ./environments/mysql.env -f docker-compose.yml -f docker-compose.dev.yml stop 
 else
-  docker-compose --project-name "template" --env-file ./docker/environments/mysql.env -f docker-compose.yml -f docker-compose.prod.yml stop --no-deps
+  docker-compose --project-name "template" --env-file ./environments/mysql.env -f docker-compose.yml -f docker-compose.prod.yml stop
 fi 
