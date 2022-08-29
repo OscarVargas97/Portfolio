c8c5ed3972e41bb6498d9072cf856b8f3e2b0851
OscarVargas97 <oscarvargas97@gmail.com>
2022-08-29 14:34:42 -0400

    Start login implementation.

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,18 +1,13 @@
 import * as services from '../services/user/user.services'
 import * as dto from '../db/dto/user.dto'
 import { HttpResponse } from '../services/shared/http.response.services'
-//import { UserAttributes, UserOutput } from '../models/user.model'
 import { Request, Response } from 'express'
 
+
 export const create = async (req: Request, res: Response): Promise<Response> => {
-
 	const id = req.body as dto.CreateUserDTO
-
 	try {
-		return HttpResponse.Ok(
-			res,
-			await services.create(id)
-		)
+		return HttpResponse.Ok(res, await services.create(id))
 	}
 	catch {
 		return HttpResponse.NotFound(res, "User not create")
@@ -22,28 +17,37 @@ export const create = async (req: Request, res: Response): Promise<Response> => 
 export const getById = async (req: Request, res: Response): Promise<Response> => {
 	const id = Number(req.params.id)
 	try {
-		return res.status(200)
-			.send(
-				await services.getById(id)
-			)
+		return HttpResponse.Ok(res, await services.getById(id))
 	}
 	catch {
 		return HttpResponse.NotFound(res, "User not found")
 	}
 }
-/*
-export const update = async (): Promise<Response> => {
-	id: number, payload: UpdateTaskDTO
-	return await services.update(id, payload)
+
+export const update = async (req: Request, res: Response): Promise<Response> => {
+	const id = Number(req.params.id)
+	const payload: dto.UpdateUserDTO = req.body
+	try {
+		return HttpResponse.Ok(res, await services.update(id, payload))
+	} catch {
+		return HttpResponse.InternalServerError(res, 'Failed to update the user')
+	}
 }
 
-export const deleteById = async (): Promise<Response> => {
-	id: number
-	const isDeleted = await services.deleteById(id)
-	return isDeleted
+export const deleteById = async (req: Request, res: Response): Promise<Response> => {
+	const id = Number(req.params.id)
+	try {
+		return HttpResponse.Ok(res, await services.deleteById(id))
+	} catch {
+		return HttpResponse.InternalServerError(res, 'Failed to delete the user')
+	}
 }
-export const getAll = async (): Promise<Response> => {
-	filters: FilterTasksDTO
-	return await services.getAll(filters)
+
+export const getAll = async (req: Request, res: Response): Promise<Response> => {
+	const filters: dto.FilterUserDTO = req.query
+	try {
+		return HttpResponse.Ok(res, await services.getAll(filters))
+	} catch {
+		return HttpResponse.InternalServerError(res, 'Failed to find all users')
+	}
 }
-*/
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -11,32 +11,16 @@ userRoutes.get('/:id', async (req: Request, res: Response) => {
 	return userController.getById(req, res)
 })
 
-
-
-/*
-
-tasksRouter.put('/:id', async (req: Request, res: Response) => {
-	const id = Number(req.params.id)
-	const payload: UpdateTaskDTO = req.body
-
-	const result = await taskController.update(id, payload)
-	return res.status(201).send(result)
+userRoutes.put('/:id', async (req: Request, res: Response) => {
+	return userController.update(req, res)
 })
 
-tasksRouter.delete('/:id', async (req: Request, res: Response) => {
-	const id = Number(req.params.id)
-
-	const result = await taskController.deleteById(id)
-	return res.status(204).send({
-		success: result
-	})
+userRoutes.delete('/:id', async (req: Request, res: Response) => {
+	return userController.deleteById(req, res)
 })
 
-tasksRouter.get('/', async (req: Request, res: Response) => {
-	const filters: FilterTasksDTO = req.query
-
-	const results = await taskController.getAll(filters)
-	return res.status(200).send(results)
+userRoutes.get('/', async (req: Request, res: Response) => {
+	return userController.getAll(req, res)
 })
-*/
+
 export default userRoutes
